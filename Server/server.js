623547f4d1f8fee910b882297d3ac3901e7c99a7
iykonical <nachiezeugwu@gmail.com>
2026-05-30 02:20:01 +0200

    force IPv4 for Gmail SMTP on Render

--- Server/server.js
+++ Server/server.js
@@ -30,6 +30,7 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false, // TLS
+  family: 4, // IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
